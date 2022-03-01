759e06a53f9c990d426b7aa0a8a96891cd80fe16
Sébastien Gilbert <sebastiengilbert73@yahoo.ca>
2022-03-01 10:57:12 -0500

    ../src/synthetic_heatmap/generators/char_string.py: Generate(): Bulk of the code

--- src/synthetic_heatmap/generators/char_string.py
+++ src/synthetic_heatmap/generators/char_string.py
@@ -9,12 +9,13 @@ import os
 import copy
 import string
 import logging
+import imutils
 
 class CharString(Generator):
     def __init__(self,
-                 font_scale_range=(0.2, 2.0),
-                 graylevel_range=(10, 240),
-                 noise_amplitude_range=(2, 20),
+                 font_scale_range=(0.5, 2.0),
+                 graylevel_range=(30, 100),
+                 noise_amplitude_range=(1, 3),
                  number_of_characters_range=(2, 20),
                  number_of_strings_range=(1, 5),
                  font_types=[cv2.FONT_HERSHEY_SIMPLEX, cv2.FONT_HERSHEY_PLAIN, cv2.FONT_HERSHEY_DUPLEX,
@@ -32,6 +33,7 @@ class CharString(Generator):
         super().__init__(parameters_dict)
         self.font_types = font_types
         self.characters = characters
+        self.rng = np.random.default_rng()
 
     def Generate(self, image_sizeHW,
                  maximum_number_of_trials=None,
@@ -47,6 +49,7 @@ class CharString(Generator):
         grayscale_img = cv2.cvtColor(background_image, cv2.COLOR_BGR2GRAY)
         generated_img = copy.deepcopy(grayscale_img)
         heatmap = np.zeros(image_sizeHW, dtype=np.uint8)
+        average_graylevel = np.mean(grayscale_img)
 
         # Generate random text
         number_of_strings = self.RandomValueInRange('number_of_strings_range', must_be_rounded=True)
@@ -57,12 +60,55 @@ class CharString(Generator):
             bottom_left = (random.randint(0, image_sizeHW[1] - 1), random.randint(0, image_sizeHW[0] - 1))
             font = random.choice(self.font_types)
             scale = self.RandomValueInRange('font_scale_range')
-            logging.debug("CharString.Generate(): scale = {}".format(scale))
-            thickness = 1
+            #logging.debug("CharString.Generate(): scale = {}".format(scale))
+            thickness = 2
             if scale > 1:
-                thickness = round(scale)
+                thickness = 1 + round(scale)
             graylevel = self.RandomValueInRange('graylevel_range', must_be_rounded=True)
+            if np.random.randn() < 0.5:
+                graylevel = int( np.clip( round(average_graylevel - graylevel), 0, 255))
+            else:
+                graylevel = int( np.clip( round(average_graylevel + graylevel), 0, 255))
+
             cv2.putText(text_img, random_string, bottom_left, font, scale, graylevel, thickness)
+
+        # Random rotation
+        theta_in_degrees = 360 * np.random.rand()
+        text_img = imutils.rotate(text_img, theta_in_degrees)
+
+        # The eroded text mask
+        _, eroded_text_mask = cv2.threshold(text_img, self.parameters_dict['graylevel_range'][0], 255, cv2.THRESH_BINARY)
+        text_erosion_kernel = np.ones((1, 1), dtype=np.uint8)
+        eroded_text_mask = cv2.erode(eroded_text_mask, text_erosion_kernel)
+
+        # Heatmap: threshold the text image
+        _, heatmap = cv2.threshold(text_img, self.parameters_dict['graylevel_range'][0] , 255, cv2.THRESH_BINARY)
+        dilation_kernel = np.ones((5, 5), dtype=np.uint8)
+        heatmap = cv2.dilate(heatmap, dilation_kernel)
+
+        boundary_kernel = np.ones((5, 5), dtype=np.uint8)
+        boundary_mask = cv2.dilate(heatmap, boundary_kernel) - heatmap
+
+        # Generate noise
+        noise_sigma = self.RandomValueInRange('noise_amplitude_range')
+        noise_img = noise_sigma * self.rng.standard_normal(image_sizeHW)
+
+        # Blur the background image, as the text background must be approximately uniform
+        blurring_size = (11, 11)
+        blurred_background_img = cv2.blur(grayscale_img, blurring_size)
+        generated_img = np.where(heatmap > 0, blurred_background_img + noise_img, grayscale_img)
+
+        blurred_generated_img = cv2.blur(generated_img, (3, 3))
+        # Scan the heatmap
+        for y in range(heatmap.shape[0]):
+            for x in range(heatmap.shape[1]):
+                if eroded_text_mask[y, x] > 0:
+                    generated_img[y, x] = text_img[y, x] + noise_img[y, x]
+                elif boundary_mask[y, x] > 0:
+                    generated_img[y, x] = blurred_generated_img[y, x] + noise_img[y, x]
+
+        blurred_generated_img = cv2.blur(generated_img, (3, 3))
+        generated_img = np.where(heatmap , blurred_generated_img, generated_img)
 
         if debug_directory is not None:
             cv2.imwrite(os.path.join(debug_directory, "CharString_Generate_text.png"), text_img)
